831b568e52d2fe9917a4372dd25357c3f069640d
jmtzt <jmttozato@gmail.com>
2020-04-22 17:24:43 -0300

    let users submit text

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -58,7 +59,7 @@ passport.use(new GoogleStrategy({
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
     function (accessToken, refreshToken, profile, cb) {
-        console.log(profile);
+        // console.log(profile);
 
         User.findOrCreate({ googleId: profile.id }, function (err, user) {
             return cb(err, user);
@@ -124,18 +125,50 @@ app.route("/register")
 
 app.route("/secrets")
     .get(function (req, res) {
-        if (req.isAuthenticated()) {
-            res.render("secrets");
-        }
-        else {
-            res.redirect("/login");
-        }
+        User.find({ "secret": { $ne: null } }, function (err, foundUsers) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                if (foundUsers) {
+                    res.render("secrets", { usersWithSecrets: foundUsers })
+                }
+            }
+        });
     });
 
 app.route("/logout")
     .get(function (req, res) {
         req.logout();
         res.redirect("/");
+    });
+
+app.route("/submit")
+    .get(function (req, res) {
+        if (req.isAuthenticated()) {
+            res.render("submit");
+        }
+        else {
+            res.redirect("/login");
+        }
+    })
+    .post(function (req, res) {
+        const submittedSecret = req.body.secret;
+
+        User.findById(req.user.id, function (err, foundUser) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                if (foundUser) {
+                    foundUser.secret = submittedSecret;
+                    foundUser.save(function () {
+                        res.redirect("/secrets");
+                    });
+                }
+            }
+        });
+
     });
 
 
